69ba6b21c4d0103508c6cf43d8668181e35aad41
Alexhr <alex.hernandez24kb@gmail.com>
2023-08-16 02:08:55 -0600

    Cambios en los modelos, serializers y rutas

---
+++ projects/urls.py
@@ -0,0 +1,17 @@
+from rest_framework import routers
+from .api import *
+
+router = routers.DefaultRouter()
+
+router.register('api/subjects', SubjectViewSet, 'subjects') 
+router.register('api/groups', GroupViewSet, 'groups')
+router.register('api/classacademies', ClassAcademyViewSet, 'classacademies')
+router.register('api/buildings', BuildingViewSet, 'buildings')
+router.register('api/classrooms', ClassRoomViewSet, 'classrooms')
+router.register('api/teachers', TeacherViewSet, 'teachers')
+router.register('api/schedules', ScheduleViewSet, 'schedules',)
+
+urlpatterns = router.urls
+
+
+
